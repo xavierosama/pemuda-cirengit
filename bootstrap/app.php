0629d2a7ae62445194c025760f984cf0fafa123c
mhrmnvl <mhrmnvl@gmail.com>
2026-06-25 21:49:09 +0700

    Log actual exceptions to stderr to debug Target class view does not exist error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,5 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->report(function (\Throwable $e) {
+            error_log('ACTUAL_ERROR: ' . $e->getMessage() . ' at ' . $e->getFile() . ':' . $e->getLine());
+        });
     })->create();
